82a431aefcf6c886c45ad0c9f2a2660aaeab630e
Pattyryke <patjoecap@gmail.com>
2023-03-10 19:25:55 -0700

    Ugh...

--- src/Console.jsx
+++ src/Console.jsx
@@ -1,25 +1,14 @@
 import React, {useRef, useLayoutEffect, Children} from "react";
+import gsap from "gsap";
 import {v4 as uuid} from "uuid";
+import Cursor from "./Cursor";
 import $ from "jQuery";
+
 function GetUniqueID() {
     const short_unique_id = uuid().slice(0.5); //.slice(0,8)
     const complete_unique_id = ('#'+short_unique_id);
     return (complete_unique_id);
 }
-
-const Cursor = () => {
-    const app = useRef();
-    const cursor = useRef();
-    useLayoutEffect(() => {
-        let ctx = gsap.context(() => {gsap.to(cursor.current, {opacity: 0, ease: "power2.inOut", repeat: -1});
-        }, app);
-        return () => ctx.revert();
-    }, []);
-    return <div className="#cursor" ref={cursor}>_</div>;
-}
-
-
-
 
 
 
@@ -61,32 +50,28 @@ export function Console(props) {
 
             let crtFlicker = gsap.fromTo(crt.current, {opacity: 0.96, zIndex: 2, repeat: -1, duration: 0.15}, {duration: 1, opacity: 0.94});
             let crtTextShadow = gsap.fromTo(crt.current, { textShadow: "-3px 1px 3px red, 3px -1px 3px blue, 0 0 3px", duration: 0.2, ease: "steps.inOut" }, { textShadow: "-1px 0px 1px red, 1px 0px 1px blue, 0 0 2px" });
-            let typeTimeline = gsap.timeline();
-            $.when($(document).ready).then(function() {
-                $('ul').each(function(i) {
-                    if ($(this).is(".consoleText")) {
-                        let cursorAnimation = gsap.fromTo(Cursor.current, 
-                            {opacity: 0, autoAlpha: 0, x: 2}, 
-                            {opacity: 1, autoAlpha: 1, duration: 0.6, repeat: -1, yoyo: true});
-                        let typeWord = gsap.to($(this), {opacity: 1, text: {value: item.value}, duration: 2, delay: 1});
-                        let cursorFadeOut = gsap.to(Cursor.current, {opacity: 0, duration: 1});
-                        
-                        typeTimeline.add(typeWord)
-                        typeTimeline.add(cursorFadeOut)
             
-                    }
-                });
+            var $eachConsoleText = $(".consoleText");
+            var $cursorElement = $(Cursor);
+            $eachConsoleText.each(function(i, element) {
+                let typeTimeline = gsap.timeline({delay:i*2});
+                typeTimeline.fromTo($cursorElement, {opacity: 0, autoAlpha: 0, x: 2}, {opacity: 1, autoAlpha: 1, duration: 0.6, repeat: -1, yoyo: true});
+                typeTimeline.to(this, {opacity: 1, text: {value: item.value}, duration: 2, delay: 1});
+                typeTimeline.to($cursorElement, {opacity: 0, duration: 1});
+                typeTimeline.play();
+                
             });
+
             let crtEffect = gsap.timeline({ repeat: -1, repeatDelay: 1.3, yoyo: true })
                 crtEffect.add(crtTextShadow),
                 crtEffect.add(crtFlicker), "<0.8";
 
 
             let masterTL = gsap.timeline()
-                masterTL.add(crtEffect),
-                masterTL.add(typeTimeline);
+                masterTL.add(crtEffect)
             
             masterTL.play();
+            
         }, app);
         return () => ctx.revert();
     }, []);
--- src/Cursor.jsx
+++ src/Cursor.jsx
@@ -1,5 +1,15 @@
-// import React, {useRef, useLayoutEffect} from "react";
+import React, {useRef, useLayoutEffect} from "react";
 
 
-// export 
-// export default Cursor
+export const Cursor = () => {
+    const app = useRef();
+    const cursor = useRef();
+    useLayoutEffect(() => {
+        let ctx = gsap.context(() => {
+            gsap.to(cursor.current, {opacity: 0, ease: "power2.inOut", repeat: -1});
+        }, cursor);
+        return () => ctx.revert();
+    }, []);
+    return <div className="#cursor" ref={cursor}>_</div>;
+}
+export default Cursor
